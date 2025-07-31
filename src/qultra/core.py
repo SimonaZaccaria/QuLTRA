d63574317ba1a9586b5ce8110a85b1747aae01c5
SimonaZaccaria <simona.zaccaria3@studio.unibo.it>
2025-07-31 16:54:38 +0200

    commented submatrix

--- src/qultra/core.py
+++ src/qultra/core.py
@@ -1183,7 +1183,7 @@ class QCircuit:
         self.show_modes()
         self.show_chi()
         return
-
+'''
     def get_Z_submatrix(self,port,f,k=0):
         z=2*np.pi*1e6*k + 1j*2*np.pi*1e9*f
         Y=self.build_total_Y_matrix(z)
@@ -1191,7 +1191,7 @@ class QCircuit:
         Z_submatrix=np.zeros((len(port),len(port)),dtype=complex)
         for i in range(len(port)):
             for j in range(len(port)):
-                Z_submatrix[i,j]=Z[port[i],port[j]]
+                Z_submatrix[i,j]=Z[port[i]-1,port[j]-1]
         return Z_submatrix
-
+'''
 
